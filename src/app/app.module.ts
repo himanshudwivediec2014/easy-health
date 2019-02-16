971bbb6c0a6b80fd8fd03726705583bd1a477e8c
Himanshu Dwivedi <himanshudwivediec2014@gmail.com>
2019-02-17 01:14:17 +0530

    desktop-UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InformationComponent } from './information/information.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InformationComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      {path:'home', component: InformationComponent},
+      {path:'', redirectTo:'home', pathMatch:'full'},
+      {path:'*', redirectTo:'home', pathMatch:'full'},
+      {path:'**', redirectTo:'home', pathMatch:'full'}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
